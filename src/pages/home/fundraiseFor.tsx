474db6438f7f2b93399b03208c72590a6650312b
DukeSmile <mitchelldev232@gmail.com>
2022-11-16 04:30:20 +0900

    style: Add background color

--- src/pages/home/fundraiseFor.tsx
+++ src/pages/home/fundraiseFor.tsx
@@ -38,32 +38,27 @@ export const FundRaiseForPage = () => {
   ];
   return (
     <div className="bg-alabaster">
-        <div className="w-[95%] md:w-[80%] mx-auto my-100 lg:py-0">
-        <p className="text-center text-38 font-bold">FundRaise for ...</p>
-        <div className="my-40">
-            <Grid container spacing={1}>
-            {
-                donatePurposes.map((purpose: donatePurposeProp, index:number) => {
-                return (
-                    <Grid item xs={12} sm={6} key={index}>
-                    <div className="shadow-default m-5 p-20 hover:bg-white rounded-10 cursor-pointer flex items-center">
-                        <div className="text-20 capitalize mr-20">
-                        <p>{purpose.title}</p>
-                        <p>{purpose.detail}</p>
+        <div className="w-[95%] md:w-[80%] mx-auto py-100 lg:py-0">
+            <p className="text-center text-38 font-bold">FundRaise for ...</p>
+            <div className="my-40">
+                <Grid container spacing={1}>
+                {
+                    donatePurposes.map((purpose: donatePurposeProp, index:number) => {
+                    return (
+                        <Grid item xs={12} sm={6} key={index}>
+                        <div className="shadow-default m-5 p-20 bg-white rounded-10 cursor-pointer flex items-center">
+                            <div className="text-20 capitalize mr-20">
+                                <p className="font-bold">{purpose.title}</p>
+                                <p>{purpose.detail}</p>
+                            </div>
+                            <img src={baseIcons[purpose.img].img} className="w-100 h-100"/>
                         </div>
-                        <img src={baseIcons[purpose.img].img} />
-                    </div>
-                    </Grid>
-                )
-                })
-            }
-            </Grid>
-        </div>
-        <div className="flex justify-center">
-            <button className={baseStyles.normalBtn}>
-            Donate Now <FontAwesomeIcon icon={faArrowRight} />
-            </button>
-        </div>
+                        </Grid>
+                    )
+                    })
+                }
+                </Grid>
+            </div>
         </div>
     </div>
   );
