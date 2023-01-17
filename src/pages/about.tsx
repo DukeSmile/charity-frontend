87b7c5283dccd9507e77f3e71d0fbb2cd81401b0
DukeSmile <mitchelldev232@gmail.com>
2023-01-17 21:20:16 +0900

    style: Add magin on `about page`

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,16 +10,18 @@ export const AboutPage = () => {
       <div className="relative bg-gradient-to-r from-algae to-seagreen w-full h-200 flex items-end justify-between overflow-hidden">
       </div>
       <div className="w-[95%] md:w-[80%] mx-auto border-1 my-70">
-        <p className="font-bold text-center text-24">Decentralized Donation Application</p>
-        {
-          aboutStrs.map((str: string, index: number) => {
-            return (
-              <p key={index} className="text-20 my-10">
-                {str}
-              </p>
-            )
-          })
-        }
+        <div className="m-15">
+          <p className="font-bold text-center text-24">Decentralized Donation Application</p>
+          {
+            aboutStrs.map((str: string, index: number) => {
+              return (
+                <p key={index} className="text-20 my-10">
+                  {str}
+                </p>
+              )
+            })
+          }
+        </div>
       </div>
     </div>
   )
